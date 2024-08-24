4caaac4a4b075176ee71e2c7b21ac9379d9da2aa
Ragul <ragulraja2808@gmail.com>
2024-08-24 16:04:01 +0530

    Update server.js to include custom route handling and 404 error handling

--- server.js
+++ server.js
@@ -5,4 +5,17 @@ const port = process.env.PORT || 3500;
 
 app.use('/', express.static(path.join(__dirname, "/public")));
 
+app.use('/', require('./routes/root'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')){
+        res.json({ message: 'Not found' });
+    } else {
+        res.type('txt').send('Not found');
+    }
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
